0f07f9ca36afcd9162ca434dc2d802615f52ffad
Raffay Shahid <raffayshahid@hotmail.com>
2024-03-21 09:22:43 -0400

    Adding a Footer to the website

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer: React.FC = () => (
+    <footer>
+      <p>Contact Us: [email]</p>
+        <p>© 2021 </p>
+      {/* Newsletter signup form */}
+    </footer>
+  );
+  
+  export default Footer;
+  
