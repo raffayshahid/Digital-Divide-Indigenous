d95fe67cb0cba9aef9b9712fbd39f4d8e8d7d24d
JerryHu87 <56461496+JerryHu87@users.noreply.github.com>
2024-03-24 11:39:10 -0400

    Create core pages

---
+++ src/components/Help/Help.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from 'next/image'; // Import Image component from Next.js
+import styles from './Help.module.css';
+
+const Help: React.FC = () => (
+  <section className={styles.help}>
+    <h2>Help Page</h2>
+
+  </section>
+);
+
+
+export default Help;
---
+++ src/components/Research/Research.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from 'next/image'; // Import Image component from Next.js
+import styles from './Research.module.css';
+
+const Research: React.FC = () => (
+  <section className={styles.research}>
+    <h2>Research Page</h2>
+
+  </section>
+);
+
+
+export default Research;
