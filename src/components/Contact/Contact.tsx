eb966c0e145a50c249cbb9371c8f9b19ac2c34ae
Raffay Shahid <raffayshahid@hotmail.com>
2024-04-03 23:38:18 -0400

    Added Chart data and the help page:

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import Image from 'next/image'; // Import Image component from Next.js
 import styles from './Contact.module.css';
+import LinkedInIcon from '@mui/icons-material/LinkedIn'; // Importing LinkedIn icon
 
 const Contact: React.FC = () => (
   <>
     <section className={styles.contact}>
-      <h2>Contact Us Page</h2>
-      <p>Muhammed Raffay Shahid: mrshahid@uwaterloo.ca</p>
-      <p>Jerry Hu: j347hu@uwaterloo.ca</p>
-      <p>Oliver Neish: oneish@uwaterloo.ca</p>
-      <p>Josh Dancey: jdancey@uwaterloo.ca</p>
+      <h2>Contact Us</h2>
+      <div>
+        <h3>Email Addresses</h3>
+        <p>Muhammed Raffay Shahid: <a href="mailto:mrshahid@uwaterloo.ca">mrshahid@uwaterloo.ca</a> <a href="https://www.linkedin.com/in/raffayshahid786/"><LinkedInIcon /></a></p>
+        <p>Jerry Hu: <a href="mailto:j347hu@uwaterloo.ca">j347hu@uwaterloo.ca</a> <a href="https://www.linkedin.com/in/jerry-hu-39b332205/"><LinkedInIcon /></a></p>
+        <p>Oliver Neish: <a href="mailto:oneish@uwaterloo.ca">oneish@uwaterloo.ca</a> <a href="https://www.linkedin.com/in/oliverneish/"><LinkedInIcon /></a></p>
+        <p>Josh Dancey: <a href="mailto:jdancey@uwaterloo.ca">jdancey@uwaterloo.ca</a> <a href="https://www.linkedin.com/in/joshua-dancey-002a28211/"><LinkedInIcon /></a></p>
+      </div>
     </section>
   </>
 );
-
 
 export default Contact;
